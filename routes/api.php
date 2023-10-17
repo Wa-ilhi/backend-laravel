0c5a3d66ede6f2a06d084958c63a73ee5abd005e
Renyl Banua <rlynebanks@gmail.com>
2023-10-18 06:06:44 +0800

    laravelUpdated

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CarouselSampleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/greeting', function () {
-    return 'Hello,World';
-});
+Route::get('/carousel', [CarouselSampleController::class, 'index']);
+Route::get('/carousel/{id}', [CarouselSampleController::class, 'show']);
